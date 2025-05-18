2736651a1f6eee5984c03ab8f9a60c041820d191
Vaibhav Lagad <vaibhav.lagad12@gmail.com>
2025-05-18 04:53:58 -0400

    day 6 - basic and advanced command practice and basic shell scripts

---
+++ Shell-scripting/ifelse.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+a=4
+b=10
+
+if [ $a > $b ]
+then
+	echo "a is greater than b"
+else
+	echo "b is greater than a"
+fi
+
+# for loop -> iterations
+
+for i in {1.30} ;
+do
+	echo $i;
+done
